2470fafa09b4d78cdc25306219f2c25e637202f9
macrespo42 <maxime.crespopro@gmail.com>
2020-03-18 17:18:20 +0100

    add git to installer

--- installer.sh
+++ installer.sh
@@ -3,10 +3,13 @@ curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update && sudo apt-get install spotify-client
 
+# add git
+sudo apt-get -y install git
+
 # add oh my zsh
 sudo apt-get -y install zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # cp conf files
 sudo cp -r .vscode ~/.vscode
-sudo cp -r .zshrc ~/.zshrc
+sudo cp -r .zshrc ~/.zshrc && source ~/.zshrc
