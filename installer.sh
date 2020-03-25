55afcff982ce6be7b1b1e78727f959e5158b760c
macrespo42 <maxime.cresopro@gmail.com>
2020-03-25 17:52:28 +0100

    installer.sh add good practices

--- installer.sh
+++ installer.sh
@@ -1,47 +1,64 @@
 #!/bin/bash
 
-bin_path="/usr/bin"
+# Install my software and config zsh / vscode
 
-# add spotify
-if [ ! -e "${bin_path}/spotify" ]; then
-    curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
-    echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-    sudo apt-get update && sudo apt-get install spotify-client
-    echo "spotify is installed now ✓"
-else
-    echo "spotify is already installed"
-fi
+install_spotify(){
+    if [ ! -e "${bin_path}/spotify" ]; then
+        curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
+        echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+        sudo apt-get update && sudo apt-get install spotify-client
+        echo "spotify is installed now ✓"
+    else
+        echo "spotify is already installed"
+    fi
+}
 
-# add git
+install_git(){
+    if [ ! -e "${bin_path}/git" ]; then
+        sudo apt-get -y install git
+        echo "git is installed now ✓"
+    else
+        echo "git already installed"
+    fi
+}
 
-if [ ! -e "${bin_path}/git" ]; then
-    sudo apt-get -y install git
-    echo "git is installed now ✓"
-else
-    echo "git already installed"
-fi
+install_vim(){
+    if [ ! -e "${bin_path}/vim" ]; then
+        sudo apt-get -y install vim
+        echo "vim is installed now ✓"
+    else
+        echo "vim is already installed"
+    fi
+}
 
-# add vim
+install_terminator(){
+    if [ ! -e "${bin_path}/vim" ]; then
+        sudo apt­-get install -y terminator
+        echo "terminator is installed now ✓"
+    else
+        echo "terminator is already installed"
+    fi
+}
 
-if [ ! -e "${bin_path}/vim" ]; then
-    sudo apt-get -y install vim
-    echo "vim is installed now ✓"
-else
-    echo "vim is already installed"
-fi
+install_oh_my_zsh(){
+    sudo apt-get -y install zsh
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+}
 
-# add terminator
-if [ ! -e "${bin_path}/vim" ]; then
-    sudo apt­-get install -y terminator
-    echo "terminator is installed now ✓"
-else
-    echo "terminator is already installed"
-fi
+set_dotfiles(){
+    sudo cp -r .vscode ~/.vscode
+    sudo cp -r .zshrc ~/.zshrc && source ~/.zshrc
+}
 
-# add oh my zsh
-sudo apt-get -y install zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+main(){
+    bin_path="/usr/bin"
+    install_spotify
+    install_git
+    install_vim
+    install_terminator
+    install_oh_my_zsh
+    set_dotfiles
+}
 
-# cp conf files
-sudo cp -r .vscode ~/.vscode
-sudo cp -r .zshrc ~/.zshrc && source ~/.zshrc
+main
+
