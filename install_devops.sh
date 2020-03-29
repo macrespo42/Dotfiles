afc7833f974e7a3d11488d93b5cf6c2868104147
macrespo42 <maxime.cresopro@gmail.com>
2020-03-29 16:43:42 +0200

    correct syntax error in devops sh

--- install_devops.sh
+++ install_devops.sh
@@ -3,7 +3,7 @@
 # install docker, docker compose, kubectl and minikube
 
 install_docker(){
-    if [ ! -e "usr/bin/docker"]; then
+    if [ ! -e "usr/bin/docker" ]; then
         sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(. /etc/os-release; echo "$UBUNTU_CODENAME") stable"
         sudo apt update
         sudo apt install docker-ce docker-compose
@@ -24,7 +24,7 @@ install_kubectl(){
 
 install_minikube(){
     curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-    chmox +x minikube
+    chmod +x minikube
     sudo mkdir -p /usr/local/bin/
     sudo mv minikube /usr/local/bin/minikube
     echo "minikube latest version, successfully installed"
